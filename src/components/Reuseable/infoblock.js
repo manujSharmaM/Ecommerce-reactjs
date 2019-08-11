8d1c124e0968b95961d89216792e0022ef6cc846
manujSharmano1@gmail.com <manujSharmano1@gmail.com>
2019-08-11 15:22:01 +0530

    InfoBlock

--- src/components/Reuseable/infoblock.js
+++ src/components/Reuseable/infoblock.js
@@ -9,7 +9,7 @@ export default function InfoBlock({heading}) {
              <Heading title={heading} />
              <div className="col-10 col-sm-8 mx-auto text-center">
                   <p className="lead text-white mb-4">
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. At um quod vel aliquam doloremque ratione sed eligendi? Ipsam blanditiis vero enim, modi incidunt alias voluptas, tenetur consequatur labore doloribus est accusamus, sed rem! Atque, deserunt quae temporibus sint voluptates ratione omnis similique. Ipsam voluptas laboriosam eveniet! Repudiandae dolore hic dolores earum illum impedit harum quisquam, libero sit. Exercitationem, ipsam doloremque?
+                      Lorem ipsum dolor, sit ame consectetur adipisicing elit. At um quod vel aliquam doloremque ratione sed eligendi? Ipsam blanditiis vero enim, modi incidunt alias voluptas, tenetur consequatur labore doloribus est accusamus, sed rem! Atque, deserunt quae temporibus sint voluptates ratione omnis similique. Ipsam voluptas laboriosam eveniet! Repudiandae dolore hic dolores earum illum impedit harum quisquam, libero sit. Exercitationem, ipsam doloremque?
                   </p>
                   <Link to="/about/">
                       <button className="btn btn-warning btn-lg">{heading}
