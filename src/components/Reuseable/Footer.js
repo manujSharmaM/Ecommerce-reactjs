b239ef2fbc10272bb198974dde3d90736b536164
Manuj Sharma <32907959+manujSharmaM@users.noreply.github.com>
2019-10-22 23:45:26 +0530

    Update Footer.js

--- src/components/Reuseable/Footer.js
+++ src/components/Reuseable/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
                    <div className="row">
                        <div className="col-12 pt-2 mx-auto text-center bg-dark
                         text-light">
-                             <h6>Powered By Manuj and Yatendra&copy; 2019</h6>
+                             <h6>Powered By Manuj &copy; 2019</h6>
                         </div> 
                    </div>
                </div>                                         
